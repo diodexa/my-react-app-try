e5f78d4445be4bfdda885097323574b61280d94b
Dio <diodexa@gmail.com>
2025-11-21 00:11:40 +0700

    perubahan setting github
    perubahan setting github

--- src/pages/RHFPage.tsx
+++ src/pages/RHFPage.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { custom, z } from "zod";
+import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
 
--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === 'build' ? '/my-react-app-try/' : '/',
+   build: {
+    outDir: "docs"
+  }
 }))
 
